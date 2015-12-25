c113f094136e953014ce7472f00b7fdb1cf7bc49
ametiste <dara@ametiste.ro>
2015-12-25 16:09:42 +0200

    codacy fixes

--- test-signals/src/main/java/org/ametiste/utils/signals/SignalsBoot.java
+++ test-signals/src/main/java/org/ametiste/utils/signals/SignalsBoot.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @EnableAutoConfiguration
--- utils-ws/src/main/java/org/ametiste/utils/ws/ExternalServiceRuntimeException.java
+++ utils-ws/src/main/java/org/ametiste/utils/ws/ExternalServiceRuntimeException.java
@@ -1,14 +1,13 @@
 package org.ametiste.utils.ws;
 
 public class ExternalServiceRuntimeException extends RuntimeException {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 2560331782176201235L;
 
 	public ExternalServiceRuntimeException(String string, Exception e) {
 		super(string, e);
 	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2560331782176201235L;
 
 }
